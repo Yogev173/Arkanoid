3ecd6e358df3cb174c8ebe750d0ecfebbc510623
Yogev173 <a0585324482@gmail.com>
2021-04-23 18:43:00 +0300

    4/23/2021 18:42, final1

--- ass3/src/Game.java
+++ ass3/src/Game.java
@@ -29,7 +29,7 @@ public class Game {
     public static final double BALL_START_Y = HEIGHT * 3 / 4;
     public static final int BALL_RADIOS = 6;
     public static final double BALL_START_VELOCITY_DX = 0;
-    public static final double BALL_START_VELOCITY_DY = -3.5;
+    public static final double BALL_START_VELOCITY_DY = -5.5;
     public static final int RANDOM_LIMIT = (int) Paddle.PADDLE_DEFAULT_WIDTH / 2;
 
     //Blocks setup
