90ccacc6ff54dccd71b9157ef3d7ab226af46352
Yogev173 <a0585324482@gmail.com>
2021-05-06 16:20:53 +0300

    5/6/2021 16:20

---
+++ ass4/src/ExpressionsTest.java
@@ -0,0 +1,33 @@
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author yogev abarbanel.
+ * Test for Expression interface.
+ */
+public class ExpressionsTest {
+
+    public static void main(String[] args) {
+        //1
+        Expression expression = new And(new And(new Var("x"),
+                new Var("y")), new And(new Var("z"), new Val(true)));
+        //2
+        System.out.println(expression.toString());
+        //3
+        Map<String, Boolean> map = new HashMap<>();
+        map.put("x", true);
+        map.put("y", false);
+        map.put("z", true);
+        try {
+            System.out.println(expression.evaluate(map));
+        } catch (Exception e) {
+            System.out.println("couldn't evaluate");
+        }
+        //4
+        System.out.println(expression.nandify());
+        //5
+        System.out.println(expression.norify());
+        //6
+        System.out.println(expression.simplify());
+    }
+}
